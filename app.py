8c135dcc2936038d938c3c61cce9b250c2d6c1da
Bente Schopman <b.l.schopman@gmail.com>
2020-01-26 14:10:18 +0100

    Non working routes but working 404 error

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask, make_response, send_file, Response
+from flask import Flask, make_response, send_file, Response, render_template
 from flask_bootstrap import Bootstrap
 from app.models import MyResponse
 
 
-app = Flask(__name__, template_folder='../templates')
-app.response_class = MyResponse
 
-app.debug = True
+def page_not_found(e):
+  return render_template('404.html'), 404
 
+def create_app(config_filename):
+    app = Flask(__name__, template_folder='../templates')
+    app.response_class = MyResponse
 
-bootstrap = Bootstrap(app)
+    app.debug = True
+    app.register_error_handler(404, page_not_found)
+
+    bootstrap = Bootstrap(app)
+    return bootstrap
+
+__name__ == "__main__":
+ app = create_app(None)
+ app.run()
--- app/routes.py
+++ app/routes.py
@@ -22,23 +22,39 @@ def example_one():
     try:
         filename = "example_one.log"
         path = '/home/bente/Documents/inholland/1920-4.2-security/workshops-securify/flask_race_conditions/app/static/logs/example_one.log'
-        uploads_class = DirFolderName(filename)
-        uploads = uploads_class.get_uploads_path
-        return send_file(path, attachment_filename=filename)
+        return send_from_directory(path, filename, as_attachment=False)
     except FileNotFoundError as fnf_error:
         print(fnf_error)
 
-@app.route('/race_conditions_example_two')
+@app.route('/example_two')
 def example_two():
     log = RaceConditionExampleTwo()
     csv = log.run_example();
     try:
         filename = "example_two.log"
         path = '/home/bente/Documents/inholland/1920-4.2-security/workshops-securify/flask_race_conditions/app/static/logs/example_two.log'
-        uploads_class = DirFolderName(filename)
-        uploads = uploads_class.get_uploads_path
-        return send_file(path,attachment_filename=filename)
+        #return send_file(path,attachment_filename=filename)
+        return send_from_directory(path,filename,as_attachment=False)
+       # return send_file(path, attachment_filename=filename)
     except FileNotFoundError as fnf_error:
         print(fnf_error)
 
 
+@app.route('/hi')
+def hello_world():
+    log = RaceConditionExampleTwo()
+    csv = log.run_example();
+    try:
+        filename = "raw_data.txt"
+        path = 'app/static/logs/raw_data.txt'
+        #return send_file(path,attachment_filename=filename)
+        return send_from_directory(path, filename, as_attachment=True)
+    # return send_file(path, attachment_filename=filename)
+    except FileNotFoundError as fnf_error:
+        print(fnf_error)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
